c8ca12fb0911d3b462891e0dd3e164bd94eb437c
k_fene_8 <mandrekarh25@gmail.com>
2023-10-14 19:01:41 +0530

    General: Setup AccountScreen.kt and SearchScreen.kt

---
+++ app/src/main/java/com/k_fene_8/sproutnative/screens/SearchScreen.kt
@@ -0,0 +1,9 @@
+package com.k_fene_8.sproutnative.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Search() {
+    Text(text = "Search")
+}
